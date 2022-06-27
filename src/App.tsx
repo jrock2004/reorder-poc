d11ef52d8b09bd59f0754e4511dacbc2440b770d
John Costanzo <jrock2004@gmail.com>
2022-06-27 12:34:57 -0400

    fix: css fixes for toggle

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ const App = (): ReactElement => {
             </h1>
             <div className="pt-2">
               <label
-                className="inline-flex relative items-center mr-5 cursor-pointer"
+                className="inline-flex relative items-center cursor-pointer"
                 htmlFor="orange-toggle"
               >
                 <input
@@ -48,7 +48,7 @@ const App = (): ReactElement => {
                   onChange={(): void => setUseDrag(!useDrag)}
                 />
                 <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-orange-600 dark:peer-focus:ring-orange-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500"></div>
-                <span className="ml-3 text-sm font-semibold text-white">Orange</span>
+                <span className="ml-3 text-xs font-semibold text-white">Toggle Drag</span>
               </label>
             </div>
           </div>
